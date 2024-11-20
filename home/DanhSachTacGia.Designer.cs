8ba2886ec4eaac61ce777859cb6fe0108b73de66
xphongmaru <671762@sv.vnua.edu.vn>
2024-11-20 14:02:44 +0700

    add manager author

---
+++ home/DanhSachTacGia.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace home
+{
+    partial class DanhSachTacGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvListAuthor = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.MaTG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenTG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QueQuan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.err = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListAuthor)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.err)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(948, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(948, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách tác giả";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(64, 79);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(116, 39);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.groupBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 64);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(948, 132);
+            this.panel3.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnCreate);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnEdit);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.txtCountry);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(948, 132);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức năng";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(243, 79);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(125, 39);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Thêm mới";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(652, 79);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(87, 39);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(431, 79);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(158, 39);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Lưu chỉnh sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(641, 30);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(220, 30);
+            this.txtCountry.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(196, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(220, 30);
+            this.txtName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Quê quán:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên tác giả: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvListAuthor);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(948, 504);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hiển thị danh sách";
+            // 
+            // dgvListAuthor
+            // 
+            this.dgvListAuthor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListAuthor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListAuthor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaTG,
+            this.TenTG,
+            this.QueQuan});
+            this.dgvListAuthor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvListAuthor.Location = new System.Drawing.Point(3, 26);
+            this.dgvListAuthor.Name = "dgvListAuthor";
+            this.dgvListAuthor.RowHeadersWidth = 51;
+            this.dgvListAuthor.RowTemplate.Height = 24;
+            this.dgvListAuthor.Size = new System.Drawing.Size(942, 475);
+            this.dgvListAuthor.TabIndex = 0;
+            this.dgvListAuthor.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListAuthor_CellClick);
+            // 
+            // panel4
+            // 
+            this.panel4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.panel4.Controls.Add(this.groupBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 196);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(948, 504);
+            this.panel4.TabIndex = 3;
+            // 
+            // MaTG
+            // 
+            this.MaTG.DataPropertyName = "MaTG";
+            this.MaTG.HeaderText = "Mã tác giả";
+            this.MaTG.MinimumWidth = 6;
+            this.MaTG.Name = "MaTG";
+            // 
+            // TenTG
+            // 
+            this.TenTG.DataPropertyName = "TenTG";
+            this.TenTG.HeaderText = "Tên tác giả";
+            this.TenTG.MinimumWidth = 6;
+            this.TenTG.Name = "TenTG";
+            // 
+            // QueQuan
+            // 
+            this.QueQuan.DataPropertyName = "QueQuan";
+            this.QueQuan.HeaderText = "Quê quán";
+            this.QueQuan.MinimumWidth = 6;
+            this.QueQuan.Name = "QueQuan";
+            // 
+            // err
+            // 
+            this.err.ContainerControl = this;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(802, 79);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 39);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // DanhSachTacGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 700);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DanhSachTacGia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DanhSachTacGia";
+            this.Load += new System.EventHandler(this.DanhSachTacGia_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListAuthor)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.err)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvListAuthor;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaTG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenTG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QueQuan;
+        private System.Windows.Forms.ErrorProvider err;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ home/DanhSachTacGia.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace home
+{
+    public partial class DanhSachTacGia : Form
+    {
+        public DanhSachTacGia()
+        {
+            InitializeComponent();
+        }
+
+        SqlDataAdapter adapter = null;
+        DataSet ds = null;
+
+        private void DanhSachTacGia_Load(object sender, EventArgs e)
+        {
+            btnEdit.Enabled = false;
+            btnDelete.Enabled = false;
+
+            HienThiDanhSach();
+
+        }
+
+        private void HienThiDanhSach()
+        {
+            DatabaseConnection dbCon = new DatabaseConnection();
+            dbCon.MoKetNoi();
+
+            string query = "select * from tacgia";
+
+            adapter = new SqlDataAdapter(query, dbCon.slqCon);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "ListAuthor");
+
+            dgvListAuthor.DataSource = ds.Tables["ListAuthor"];
+
+            dbCon.DongKetNoi();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (FormValidate())
+            {
+                            DataRow row = ds.Tables["ListAuthor"].NewRow();
+            row["MaTG"] = setMaTG();
+            row["TenTG"] = txtName.Text.Trim();
+            row["QueQuan"] = txtCountry.Text.Trim();
+
+            ds.Tables["ListAuthor"].Rows.Add(row);
+
+            int kq = adapter.Update(ds.Tables["ListAuthor"]);
+            if (kq > 0)
+            {
+                MessageBox.Show("Thêm tác giả thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                HienThiDanhSach();
+            }
+            else
+            {
+                MessageBox.Show("Thêm tác giả không thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            txtCountry.Text = "";
+            txtName.Text = "";
+            }
+        }
+
+        private string setMaTG()
+        {
+            string newMaTG="";
+            DatabaseConnection dbCon = new DatabaseConnection();
+            dbCon.MoKetNoi();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "select top 1 MaTG from TacGia order by MaTG desc";
+            sqlCmd.Connection = dbCon.slqCon;
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            if (reader.Read()) {
+                string maTG = reader.GetString(0);
+                // Tách phần số từ mã cuối cùng (bỏ phần tiền tố "TG")
+                string number = maTG.Substring(2);
+                int num = int.Parse(number);// chuyển chuỗi thành số
+                num++;
+                newMaTG = "TG" + num.ToString("D3");
+
+            }
+            reader.Close();
+            dbCon.DongKetNoi();
+            return newMaTG;
+        }
+
+        int vt = -1;
+        private void dgvListAuthor_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            vt = e.RowIndex;
+            if (vt == -1) return;
+
+            if (vt < ds.Tables["ListAuthor"].Rows.Count)
+            {
+                //lay du lieu cua dong duoc chon
+                DataRow row = ds.Tables["ListAuthor"].Rows[vt];
+                txtName.Text = row["TenTG"].ToString();
+                txtCountry.Text = row["QueQuan"].ToString();
+                btnEdit.Enabled = true;
+                btnDelete.Enabled = true;
+            }
+            else if (vt == ds.Tables["ListAuthor"].Rows.Count)
+            {
+                txtCountry.Text = "";
+                txtName.Text = "";
+            }
+        }
+
+        private bool FormValidate()
+        {
+            bool isValid = true;
+
+            if (string.IsNullOrWhiteSpace(txtName.Text.Trim()))
+            {
+                isValid = false;
+                err.SetError(txtName, "Tên tác giả không được để trống!");
+                txtName.Focus();
+            }
+            else
+            {
+                err.SetError(txtName, null);
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCountry.Text.Trim()))
+            {
+                isValid = false;
+                err.SetError(txtCountry, "Quê quán không được để trống!");
+                txtCountry.Focus();
+            }
+            else
+            {
+                err.SetError(txtCountry, null);
+            }
+
+            return isValid;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (FormValidate())
+            {
+                DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn sửa thông tin tác giả này không?", "Hộp thoại", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    DataRow row = ds.Tables["ListAuthor"].Rows[vt];
+                    row.BeginEdit();
+                    row["MaTG"] = row["MaTG"];
+                    row["TenTG"] = txtName.Text.Trim();
+                    row["QueQuan"] = txtCountry.Text.Trim();
+                    row.EndEdit();
+
+                    int kq = adapter.Update(ds.Tables["ListAuthor"]);
+                    if (kq > 0)
+                    {
+                        MessageBox.Show("Sửa tác giả thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        HienThiDanhSach();
+                        btnEdit.Enabled = false;
+                        btnDelete.Enabled = false;
+                        txtName.Text = "";
+                        txtCountry.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sửa tác giả không thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //if(vt==)
+            DataRow row = ds.Tables["ListAuthor"].Rows[vt];
+
+            DialogResult dialog = MessageBox.Show("Bạn có thực sự muốn xóa tác giả: '" + row["TenTG"] +"' không?", "Hộp thoại", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+            {
+                row.Delete();
+
+                int kq = adapter.Update(ds.Tables["ListAuthor"]);
+
+                if (kq > 0)
+                {
+                    MessageBox.Show("Xóa tác giả thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnEdit.Enabled = false;
+                    btnDelete.Enabled = false;
+                    txtCountry.Text = "";
+                    txtName.Text = "";
+                    HienThiDanhSach();
+                }
+                else
+                {
+                    MessageBox.Show("Xóa tác giả không thành công!", "Hộp thoại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text.Trim() != "" && txtCountry.Text.Trim()=="")
+            {
+                TimKiemTheoTenTG();
+            }
+            else if (txtName.Text.Trim() == "" && txtCountry.Text.Trim() != "")
+            {
+                TimKiemTheoQueQuan();
+            }
+            else if (txtName.Text.Trim() != "" && txtCountry.Text.Trim() != "")
+            {
+                TimKiemTheoTenTG();
+            }
+            else
+            {
+                HienThiDanhSach();
+            }
+        }
+
+        private void TimKiemTheoTenTG()
+        {
+            DatabaseConnection dbCon = new DatabaseConnection();
+            dbCon.MoKetNoi();
+
+            string query = "select * from tacgia where TenTG like N'%" +txtName.Text.Trim() +"%'";
+
+            adapter = new SqlDataAdapter(query, dbCon.slqCon);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "ListAuthor");
+
+            dgvListAuthor.DataSource = ds.Tables["ListAuthor"];
+
+            dbCon.DongKetNoi();
+        }
+        private void TimKiemTheoQueQuan()
+        {
+            DatabaseConnection dbCon = new DatabaseConnection();
+            dbCon.MoKetNoi();
+
+            string query = "select * from tacgia where QueQuan like N'%" + txtCountry.Text.Trim() + "%'";
+
+            adapter = new SqlDataAdapter(query, dbCon.slqCon);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "ListAuthor");
+
+            dgvListAuthor.DataSource = ds.Tables["ListAuthor"];
+
+            dbCon.DongKetNoi();
+        }
+    }
+}
