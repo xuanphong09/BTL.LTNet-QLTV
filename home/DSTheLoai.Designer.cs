e567df880318c99244fdb20c59f8a3310391382b
Nguyễn Phương Linh <linhnguyen1104cod@gmail.com>
2024-12-27 16:38:04 +0700

    fix button the loai

--- home/DSTheLoai.Designer.cs
+++ home/DSTheLoai.Designer.cs
@@ -136,68 +136,85 @@
             // 
             // btnSua
             // 
+            this.btnSua.Image = global::home.Properties.Resources.icons8_edit_property_32;
             this.btnSua.Location = new System.Drawing.Point(350, 31);
             this.btnSua.Margin = new System.Windows.Forms.Padding(4);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(100, 49);
             this.btnSua.TabIndex = 8;
             this.btnSua.Text = "&Sửa";
+            this.btnSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSua.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnSua.UseVisualStyleBackColor = true;
             this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnHuy
             // 
+            this.btnHuy.Image = global::home.Properties.Resources.icons8_clear_32;
             this.btnHuy.Location = new System.Drawing.Point(636, 31);
             this.btnHuy.Margin = new System.Windows.Forms.Padding(4);
             this.btnHuy.Name = "btnHuy";
             this.btnHuy.Size = new System.Drawing.Size(113, 49);
             this.btnHuy.TabIndex = 7;
             this.btnHuy.Text = "Hủ&y";
+            this.btnHuy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHuy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnHuy.UseVisualStyleBackColor = true;
             this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
             // btnThoat
             // 
+            this.btnThoat.BackColor = System.Drawing.Color.Red;
             this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnThoat.Location = new System.Drawing.Point(792, 31);
             this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
             this.btnThoat.Name = "btnThoat";
             this.btnThoat.Size = new System.Drawing.Size(124, 49);
             this.btnThoat.TabIndex = 6;
             this.btnThoat.Text = "Th&oát";
-            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.UseVisualStyleBackColor = false;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // btnXoa
             // 
+            this.btnXoa.Image = global::home.Properties.Resources.icons8_delete_32;
             this.btnXoa.Location = new System.Drawing.Point(194, 31);
             this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(113, 49);
             this.btnXoa.TabIndex = 5;
             this.btnXoa.Text = "&Xóa";
+            this.btnXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnXoa.UseVisualStyleBackColor = true;
             this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnLuu
             // 
+            this.btnLuu.Image = global::home.Properties.Resources.icons8_save_32;
             this.btnLuu.Location = new System.Drawing.Point(493, 31);
             this.btnLuu.Margin = new System.Windows.Forms.Padding(4);
             this.btnLuu.Name = "btnLuu";
             this.btnLuu.Size = new System.Drawing.Size(100, 49);
             this.btnLuu.TabIndex = 4;
             this.btnLuu.Text = "&Lưu ";
+            this.btnLuu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLuu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnLuu.UseVisualStyleBackColor = true;
             this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // btnThem
             // 
+            this.btnThem.Image = global::home.Properties.Resources.icons8_plus_32;
             this.btnThem.Location = new System.Drawing.Point(29, 31);
             this.btnThem.Margin = new System.Windows.Forms.Padding(4);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(122, 49);
             this.btnThem.TabIndex = 3;
             this.btnThem.Text = "T&hêm ";
+            this.btnThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnThem.UseVisualStyleBackColor = true;
             this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
@@ -244,12 +261,15 @@
             // 
             // btnTimKiem
             // 
+            this.btnTimKiem.Image = global::home.Properties.Resources.icons8_search_32;
             this.btnTimKiem.Location = new System.Drawing.Point(104, 117);
             this.btnTimKiem.Margin = new System.Windows.Forms.Padding(4);
             this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(138, 35);
+            this.btnTimKiem.Size = new System.Drawing.Size(138, 49);
             this.btnTimKiem.TabIndex = 2;
             this.btnTimKiem.Text = "&Tìm Kiếm";
+            this.btnTimKiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTimKiem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnTimKiem.UseVisualStyleBackColor = true;
             this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
             // 
